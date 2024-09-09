f7b790eeee3384a51b4961aa0dd54aabb66d56b1
joaodelabio <joaogabriel.delabio@gmail.com>
2024-09-09 10:44:31 -0300

    sim

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-});
-
 const date = new Date();
 const diaSemana = document.getElementById("dia-semana");
 const dataAtual = document.getElementById("data-atual");
@@ -23,11 +17,42 @@ dialogData.textContent = getCurrentDate();
 const dialogHora = document.getElementById("dialog-hora");
 dialogHora.textContent = getCurrentTime();
 
+const dialogEntrada = document.getElementById("entrada-dialog");
+dialogEntrada.addEventListener("click", () => {
+    saveRegisterLocalStorage(JSON.stringify(getObjectRegister("entrada")));
+});
+
+const dialogSaida = document.getElementById("saida-dialog");
+dialogSaida.addEventListener("click", () => {
+    saveRegisterLocalStorage(JSON.stringify(getObjectRegister("saida")));
+})
+
+function getObjectRegister(registerType) {
+
+    ponto = {
+        "date": getCurrentDate(),
+        "time": getCurrentTime(),
+        "location": getUserLocation(),
+        "id": 1,
+        "type": registerType
+    }
+    return ponto;
+}
 
 const fecharDialog = document.getElementById("fechar-dialog");
 fecharDialog.addEventListener("click", () => {
     dialogPonto.close();
 })
+
+// salvar e recuperar um array de objetos ao inves de um objeto
+
+function saveRegisterLocalStorage(register) {
+    localStorage.setItem("register", register);
+}
+
+function getRegisterLocalStorage(key) {
+    // prox aula
+}
 
 function register() {
     dialogPonto.showModal();
@@ -50,6 +75,19 @@ function getCurrentDate() {
 function getWeekDay() {
     const weekday = ["Domingo-feira", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado-feira"];
     return weekday[date.getDay()];
+}
+
+let locationUser = {};
+
+function getUserLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        let userLocation = {
+            "lat":position.coords.latitude,
+            "long": position.coords.longitude
+        }
+        locationUser = userLocation;
+        console.log(locationUser);
+    });    
 }
 
 updateContentHour();
